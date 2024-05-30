6f63029d1deb982fbd2a165b7c16484e8082eb7e
eneagizzarelli <s314887@studenti.polito.it>
2024-05-30 14:56:57 +0200

    fix: minor fixes

--- scripts/configDENDRITE.sh
+++ scripts/configDENDRITE.sh
@@ -13,27 +13,5 @@ mkdir -p ${DENDRITE_path}/data
 
 ${DENDRITE_path}/scripts/downloadGeoLiteDB.sh
 
-sudo chown -R root:root ${DENDRITE_path}
+sudo chown -R enea:enea ${DENDRITE_path}
 sudo chmod -R 700 ${DENDRITE_path}
-
-sudo chgrp nonroot ${DENDRITE_path}
-sudo chmod 710 ${DENDRITE_path}
-sudo chgrp nonroot ${DENDRITE_path}/src
-sudo chmod 710 ${DENDRITE_path}/src
-
-sudo chgrp nonroot ${DENDRITE_path}/src/DENDRITE_login.py
-sudo chmod 710 ${DENDRITE_path}/src/DENDRITE_login.py
-sudo chgrp nonroot ${DENDRITE_path}/src/DENDRITE_logout.py
-sudo chmod 710 ${DENDRITE_path}/src/DENDRITE_logout.py
-sudo chgrp nonroot ${DENDRITE_path}/src/client_data.py
-sudo chmod 710 ${DENDRITE_path}/src/client_data.py
-
-sudo chgrp nonroot ${DENDRITE_path}/scripts
-sudo chmod 710 ${DENDRITE_path}/scripts
-sudo chgrp nonroot ${DENDRITE_path}/logs
-sudo chmod 710 ${DENDRITE_path}/logs
-sudo chgrp nonroot ${DENDRITE_path}/data
-sudo chmod 710 ${DENDRITE_path}/data
-
-sudo chmod 4755 ${DENDRITE_path}/scripts/run_dendrite_login.sh
-sudo chmod 4755 ${DENDRITE_path}/scripts/run_dendrite_logout.sh
