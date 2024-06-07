b03e38c060bd447327b4703cac9d7eab388243d1
eneagizzarelli <s314887@studenti.polito.it>
2024-06-07 15:21:34 +0200

    fix: minor fixes

--- docker_entrypoint_scripts/sql_init.sql
+++ docker_entrypoint_scripts/sql_init.sql
@@ -1,4 +1,5 @@
-USE mysql;
-UPDATE user SET host = 'localhost' WHERE user = 'enea' AND host = '%';
-UPDATE user SET host = 'localhost' WHERE user = 'root' AND host = '%';
+UPDATE mysql.user SET host = 'localhost' WHERE user = 'enea' AND host = '%';
+
+DELETE FROM mysql.user WHERE user = 'root' AND host = '%';
+
 FLUSH PRIVILEGES;
